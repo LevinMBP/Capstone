73b0119d9e032025fc39ce3e14b8b995411d1432
Krutik Bhatt <krutikbhatt95@gmail.com>
2024-10-21 22:41:46 -0400

    [+] #9 User can see AboutUs Page. As of now we have opened temporarily Google Page.

--- app/src/main/java/joseph/example/myapplication/general/AboutActivity.kt
+++ app/src/main/java/joseph/example/myapplication/general/AboutActivity.kt
@@ -1,6 +1,9 @@
 package joseph.example.myapplication.general
 
 import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import joseph.example.myapplication.R
 
@@ -8,5 +11,20 @@ class AboutActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        // Initialize the WebView
+        val webView: WebView = findViewById(R.id.webView)
+
+        // Enable JavaScript (if needed)
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+
+        // Set WebViewClient to prevent opening the browser
+        webView.webViewClient = WebViewClient()
+
+        // Load a URL
+        webView.loadUrl("https://www.google.com")
+
+
     }
 }
